5f2ad07c754e7fe68df367ad443c6fff578bb3a4
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-06-16 21:19:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -72,4 +72,6 @@ def buku_tamu():
     return html_template
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
